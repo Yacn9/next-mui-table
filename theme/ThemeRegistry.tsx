d5fb7fa411096f4460042ebe0029fedc3a54cb2a
YaCn Naghibi <yasin.naghibi9@gmail.com>
2023-08-24 22:29:55 +0330

    chore(theme): theme registry and emotion cache added

---
+++ theme/ThemeRegistry.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeOptions, ThemeProvider } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+import { NextAppDirEmotionCacheProvider } from './EmotionCache';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
+
+const themeOptions: ThemeOptions = {
+  typography: {
+    fontSize: 12,
+    fontFamily: roboto.style.fontFamily,
+  },
+  palette: {
+    background: {
+      default: '#fff',
+    },
+    primary: {
+      main: '#000',
+    },
+    text: {
+      primary: '#000',
+    },
+  },
+};
+
+const theme = createTheme(themeOptions);
+
+export default function ThemeRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <NextAppDirEmotionCacheProvider options={{ key: 'mui' }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </NextAppDirEmotionCacheProvider>
+  );
+}
